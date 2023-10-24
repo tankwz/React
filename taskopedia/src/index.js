32bdc3eb30d295a02a8a41dad5a493c803ddd876
tankw <tantk113@gmail.com>
2023-10-24 17:58:43 +0700

    bootstrap darkly + first component

--- taskopedia/src/index.js
+++ taskopedia/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './darkly.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {' '}
-    <div></div>{' '}
-  </React.StrictMode>
-);
+
+function MainBody() {
+  return (
+    <div className="container mt-5">
+      <p>p tag</p>
+      <ul className="list-group ">
+        <li className="list-group-item ">item 1</li>
+        <li className="list-group-item ">item 2</li>
+      </ul>
+    </div>
+  );
+}
+
+root.render(<MainBody />);
