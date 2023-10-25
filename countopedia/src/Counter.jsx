1f892828386866a894cb02775ec3b2f8b62da0d8
tankw <tantk113@gmail.com>
2023-10-25 12:05:46 +0700

    class compo, state, setstate, set state async problem

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -1,6 +1,54 @@
 import React from 'react';
 export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.defense = this.defense.bind(this);
+    this.state = {
+      count: 0,
+      result: '',
+      previous: '',
+    };
+  }
+  attack = () => {
+    this.setState((state) => {
+      return {
+        count: state.count + 100,
+      };
+    });
+    this.setState((state) => {
+      return {
+        count: state.count + 5,
+      };
+    });
+  };
+  defense = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+    this.setState((previousS) => ({
+      count: previousS.count - 2,
+    }));
+  };
+
   render() {
-    return <div> counter</div>;
+    return (
+      <div className="row justify-content-center  d-flex">
+        <h1 className="col-12 text-center ">Counter: {this.state.count}</h1>
+        <div className="col-12 text-center">
+          <button
+            onClick={this.attack}
+            style={{ width: '200px', height: '200px' }}
+          >
+            +
+          </button>
+          <button
+            onClick={this.defense}
+            style={{ width: '200px', height: '200px' }}
+          >
+            -
+          </button>
+        </div>
+      </div>
+    );
   }
 }
