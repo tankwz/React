f9379533f3d82e528313838076496079b7b50e46
tankw <tantk113@gmail.com>
2023-10-27 16:02:22 +0700

    wactopedia

---
+++ watchopedia/src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import './darkly.css';
+import Header from '../../watchopedia/src/Components/Layout/Header';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <Header></Header>
+  </div>
+);
