99c74fa263258e6412493d2e26ebff98567f0d0c
tankw <tantk113@gmail.com>
2023-10-28 19:45:44 +0700

    Reduxopedia

---
+++ reduxopedia/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './darkly.css';
+import Header from './app/layout/Header';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Header></Header>);
